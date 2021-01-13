d8e74fe242bc9a77926149998823816a62c364c7
Gianpaolo <pao.barga@gmail.com>
2021-01-13 17:31:07 +0100

    commit finale: bonus fatto

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
-/* tentativo di riduzione ad unica funzione dei condizionali chiamati con next e prev, ma problema nel passare funzione jquery concatenata.
-// function carousel(anchorClass1, anchorClass2, jfn) {
-//     if($("img.active").hasClass(anchorClass1)) {
-//         $("img.active").removeClass("active")
-//         $("img." + anchorClass2).addClass("active")
-//     } else {
-//         $("img.active")
-//         .removeClass("active")
-//         jfn
-//         .addClass("active");
-//     }
-// }
-
-// $(document).ready(function() {
-//         $("div.next").click(function() {
-//             carousel("last", "first", $next())
-//         })
-    
-//         $("div.prev").click(function() {
-//             carousel("first", "last", next())
-    
-//         })
-//     })
-*/
-
 $(document).ready(function() {
-    $("div.next").click(function() {
-
+    $("div.next").click(function() { 
+        //tutte le classi .fa-circle sono relative al bonus, per la versione base basta toglierle e lasciare il resto così com'è.
         if($("img.active, .fa-circle.active").hasClass("last")) {
             $("img.active, .fa-circle.active").removeClass("active")
             $("img.first, .fa-circle.first").addClass("active")
@@ -51,4 +26,41 @@ $(document).ready(function() {
         }
 
     })
+
+    //_bonus
+    $(".fa-circle").click(function() {
+        $(".fa-circle").removeClass("active");
+        $(this).addClass("active");
+
+        var i = $(".fa-circle").index(this) + 1;
+        $("img").removeClass("active");
+        $("img:nth-child(" + i + ")").addClass("active"); 
+    })
+    //_/bonus
+
 })
+
+/* tentativo di riduzione ad unica funzione dei condizionali chiamati con next e prev, ma problema nel passare funzione jquery concatenata.
+// function carousel(anchorClass1, anchorClass2, jfn) {
+//     if($("img.active").hasClass(anchorClass1)) {
+//         $("img.active").removeClass("active")
+//         $("img." + anchorClass2).addClass("active")
+//     } else {
+//         $("img.active")
+//         .removeClass("active")
+//         jfn
+//         .addClass("active");
+//     }
+// }
+
+// $(document).ready(function() {
+//         $("div.next").click(function() {
+//             carousel("last", "first", $next())
+//         })
+    
+//         $("div.prev").click(function() {
+//             carousel("first", "last", next())
+    
+//         })
+//     })
+*/
